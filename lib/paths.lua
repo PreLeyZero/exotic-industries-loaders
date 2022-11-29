8d235e08fa8475c58a27ec5e1ee7e8d690778321
PreLeyZero <Paul.Zschoppe@gmx.de>
2022-11-29 21:17:10 +0100

    setup mod structure

---
+++ lib/paths.lua
@@ -0,0 +1,11 @@
+-- stores commonly used paths esp. for graphics
+
+--====================================================================================================
+--PATHS
+--====================================================================================================
+
+ei_loaders_path = "__exotic-industries-loaders__/"
+
+ei_loaders_entity_path = ei_loaders_path.."graphics/entities/"
+ei_loaders_item_path = ei_loaders_path.."graphics/items/"
+ei_loaders_tech_path = ei_loaders_path.."graphics/techs/"
